8cc7e09c9a38476bb0d462c487d872c3dc6c69b3
Jeremiah Buckley <jeremiah.buckley@gmail.com>
2016-11-07 22:47:03 -0500

    added more information about the electionId for the candidates. added the option to select and election for a candidate.

--- src/app/components/candidate-add.component.js
+++ src/app/components/candidate-add.component.js
@@ -1,26 +1,53 @@
 class CandidateAddController {
-  constructor($log) {
+  constructor(ElectionsService, $log) {
+    this.ElectionsService = ElectionsService;
     this.$log = $log;
+    this.selectedElection = null;
+    const self = this;
+    this.elections = this.ElectionsService.query().$promise.then((result, err) => {
+      if (err) {
+        self.$log.log(err);
+      } else {
+        self.elections = result;
+        if (self.elections.length > 0) {
+          self.selectedElection = self.elections[0];
+        }
+      }});
     this.name = "";
   }
 
   save() {
     if (this.onSave) {
       const nm = this.name;
-      this.name = "";
-      this.onSave({candidateData: {name: nm, value: 0}});
+      const eId = this.selectedElection._id;
+      this.reset();
+      this.onSave({candidateData: {name: nm, value: 0, electionId: eId}});
     }
   }
 
   cancel() {
     if (this.onCancel) {
-      this.name = "";
+      this.reset();
       this.onCancel();
     }
   }
+
+  reset() {
+    this.name = "";
+    const self = this;
+    this.elections = this.ElectionsService.query().$promise.then((result, err) => {
+      if (err) {
+        self.$log.log(err);
+      } else {
+        self.elections = result;
+        if (self.elections.length > 0) {
+          self.selectedElection = self.elections[0];
+        }
+      }});
+  }
 }
 
-CandidateAddController.$inject = ['$log'];
+CandidateAddController.$inject = ['ElectionsService', '$log'];
 
 export const candidateAdd = {
   template: require('./candidate-add.html'),
